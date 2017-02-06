2734d9a1b0b08d4b29d1df0aa37e2667cf4e7379
Carole-Anne <caroleanne07@live.fr>
2017-02-06 12:34:41 +0100

    Add files via upload

--- src/tests/testRepository.java
+++ src/tests/testRepository.java
@@ -3,14 +3,13 @@ package tests;
 import static org.junit.Assert.*;
 
 import java.util.List;
-
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
-
 import org.junit.Test;
-
 import Entities.Couleur;
+import Entities.Proprietaire;
 import Repositories.RepositoryCouleur;
+import Repositories.RepositoryGroupe;
 import Services.CreateEntityManagerFactory;
 
 public class testRepository {
@@ -31,8 +30,35 @@ public class testRepository {
 		RepositoryCouleur repColor = new RepositoryCouleur(em);
 		List<Couleur> colors = repColor.getAll();
 		Couleur c = colors.get(0);
-		String couleur = c.getCouleur();
+		String couleur = c.getNom();
 		assertEquals("Amande", couleur);
+	}
+	
+	@Test
+	public void testIsNotFind(){
+		EntityManagerFactory emf = CreateEntityManagerFactory.getInstance();
+		EntityManager em = emf.createEntityManager();
+		RepositoryGroupe repGroupe = new RepositoryGroupe(em);
+		assertTrue(repGroupe.getNameFind("Doré"));
+		assertFalse(repGroupe.getNameFind("Pure Doré"));
+	}
+	
+	@Test
+	public void testAddRemoveProp(){
+		EntityManagerFactory emf = CreateEntityManagerFactory.getInstance();
+		EntityManager em = emf.createEntityManager();
+		RepositoryGroupe repGroupe = new RepositoryGroupe(em);
+		
+		Proprietaire prop = new Proprietaire();
+		prop.setNom("Test");
+		
+		repGroupe.add(prop);
+		//Groupe "Test" existe
+		assertFalse(repGroupe.getNameFind("Test"));
+		
+		repGroupe.remove(prop);
+		//Groupe "Test" n'existe pas
+		assertTrue(repGroupe.getNameFind("Test"));
 	}
 
 }
